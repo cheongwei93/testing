20e244bc72d2b70a99b5c7f21549fc0c7609e441
Cheong <cheongwei06152@hotmail.com>
2020-12-03 14:15:35 +0800

    ok

--- table.php
+++ table.php
@@ -20,16 +20,8 @@
                     <th>Menara Standard Chartered, TPM</th>
                     <th>03-22441234</th>
                 </tr>
-                <?php
-                    $connectionInfo = array("UID" => "tp046484", "pwd" => "Abcd1234", "Database" => "simpleweb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-                    $serverName = "tcp:simplewebtp046484.database.windows.net,1433";
-                    $conn = sqlsrv_connect($serverName, $connectionInfo);
-                    if(!$conn){
-                        die("Error Connection: ".sqlsrv_errors());
-                    }else{
-                        echo "Success"
-                    }
-                ?>
+                
+               
 
             </table>
         </center>
